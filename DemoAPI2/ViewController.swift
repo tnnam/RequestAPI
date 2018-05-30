9ef669a9e661de3270b56b08011f9c38e9ef9f43
Tran Ngoc Nam <tranngocnam798@gmail.com>
2018-05-30 10:41:43 +0700

    Initial Commit

---
+++ DemoAPI2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoAPI2
+//
+//  Created by Tran Ngoc Nam on 5/30/18.
+//  Copyright © 2018 Tran Ngoc Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
